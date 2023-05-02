4e5fd53b32225387fa86799443b324cec0093106
ferka123 <ferka911@gmail.com>
2023-05-02 13:57:37 +0300

    feat: search ui

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -3,16 +3,97 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import { styled, alpha } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
+import { useAppDispatch, useAppSelector } from '../../redux/store';
+import { setSearch } from '../../redux/features/feedSlice';
+import { FormEvent, useState } from 'react';
+import { Stack } from '@mui/material';
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25)
+  },
+  marginLeft: 0,
+  width: '100%',
+  flexGrow: 1,
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto'
+  }
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center'
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '30ch',
+      '&:focus': {
+        width: '40ch'
+      }
+    }
+  }
+}));
 
 const Header = () => {
+  const defSearchValue = useAppSelector((store) => store.feedState.search);
+  const [searchValue, setSearchValue] = useState(defSearchValue);
+  console.log(defSearchValue, searchValue);
+  const dispatch = useAppDispatch();
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    dispatch(setSearch(searchValue));
+  };
+
   return (
     <Box sx={{ flexGrow: 1, marginBottom: 3 }}>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            RSS Feed Grabber
+          <Typography
+            noWrap
+            variant="h6"
+            component="div"
+            sx={{ display: { xs: 'none', sm: 'block' }, marginRight: '20px' }}
+          >
+            RSS Feed
           </Typography>
-          <Button color="inherit">Login</Button>
+          <form onSubmit={handleSubmit}>
+            <Search>
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder="Search…"
+                value={searchValue}
+                onChange={(e) => setSearchValue(e.target.value)}
+                inputProps={{ 'aria-label': 'search' }}
+              />
+            </Search>
+          </form>
+          <Stack sx={{ flexGrow: 1 }}>
+            <Button color="inherit" sx={{ marginLeft: 'auto' }}>
+              Login
+            </Button>
+          </Stack>
         </Toolbar>
       </AppBar>
     </Box>
