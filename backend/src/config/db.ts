6a5fed34e5c7b2d6779194d2cb368b555f8a65f4
ferka123 <ferka911@gmail.com>
2023-05-04 14:41:40 +0300

    refactor: get mongo uri from env

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,17 +1,9 @@
 import mongoose from 'mongoose';
 
-const user = process.env.MONGODB_USERNAME;
-const pass = process.env.MONGODB_PASSWORD;
-const host = process.env.MONGODB_HOST;
-const dbname = process.env.MONGODB_DATABASE_NAME;
-
-const MONGO_URI = `mongodb://${user}:${pass}@${host}:27017/${dbname}?authMechanism=DEFAULT&authSource=admin`;
-
 const connectDB = async () => {
   try {
-    console.log(MONGO_URI);
     mongoose.set('strictQuery', false);
-    const conn = await mongoose.connect(MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGODB_URI as string);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     if (error instanceof Error) console.log(error.message);
