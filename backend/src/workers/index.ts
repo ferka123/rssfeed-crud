1a4fa7f29b0df62ade4f4af5ae2788157e9b7457
ferka123 <ferka911@gmail.com>
2023-05-05 10:08:49 +0300

    feat: worker error handler and restarter

--- backend/src/workers/index.ts
+++ backend/src/workers/index.ts
@@ -3,4 +3,12 @@ import path from 'path';
 
 const ext = process.env.NODE_ENV === 'production' ? 'js' : 'ts';
 
-export const startRssGrabber = () => new Worker(path.resolve(__dirname, `./rssGrabber.${ext}`));
+export const startRssGrabber = () => {
+  const worker = new Worker(path.resolve(__dirname, `./rssGrabber.${ext}`));
+  worker.on('error', (e) => {
+    console.log('RSS Feed Grabber Worker Error:', e);
+    worker.terminate();
+    console.log('Restarting...');
+    setTimeout(startRssGrabber, 60000);
+  });
+};
